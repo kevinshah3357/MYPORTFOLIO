73942dabf034f0d151ec8199e4dced6616781260
kevinshah3357 <shahkevin033@gmail.com>
2024-10-14 00:25:15 +0530

    xs

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,56 @@
+import Lottie from "lottie-react";
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import Coder from '../LottieFiles/coder.json';
+import Skills from './Skills.js';
 
 const About = () => {
   return (
-    <section id="about">
-      <h2>About Me</h2>
-      <p>I am a passionate frontend developer with experience in building responsive web applications using React.</p>
-    </section>
-  );
-};
+    <>
+      <div className='AboutPage'>
+        <div className='AboutText'>
+          <h1 className='AboutTextHeading' >Get to <b>know</b> me!</h1>
+          <p>
+            Hi, my name is <b>KEVIN SHAH</b> and I am from Gujarat, India.
+            I'm a <b>MERN stack web developer</b> and a final year college student pursuing <b>BE in IT</b> from <b>LD College of Engineering</b>. <br/><br/>
+            I have done an internship as a <b>full stack web developer</b> at TatvaSoft
+            I love to create original projects with beautiful designs, you can check out some of my work in the projects section.<br/><br/>
 
-export default About;
+          </p>
+        </div>
+
+        <div>
+          <Tilt>
+              <Lottie 
+              className="illustration" 
+              animationData={Coder} 
+              loop={true} 
+            />
+          </Tilt>
+        </div>
+
+      </div>
+      
+      <h1 className='SkillsHeading'>Professional Skillset</h1>
+      <div className='skills'>
+        
+        <Skills skill='React' />
+        <Skills skill='Node' />
+        <Skills skill='Express' />
+        <Skills skill='MongoDb' />
+        <Skills skill='Git' />
+        <Skills skill='Github' />
+        <Skills skill='Javascript' />
+        <Skills skill='C++' />
+        <Skills skill='Postman' />
+        <Skills skill='Figma' />
+        <Skills skill='Vercel' />
+        <Skills skill='Npm' />
+        <Skills skill='Bootstrap'/>
+        
+      </div>
+    </>
+  )
+}
+
+export default About
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { GrMail } from "react-icons/gr";
+import { SiLeetcode } from "react-icons/si";
 
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+      <h4>Developed by Devansh Sahni</h4>
+      <h4>Copyright &copy; 2023 DS</h4>
+      <div className='footerLinks'>
+        <a href="https://github.com/kevinshah3357" target='_blank'><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/kevin-shah-940a6922b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank'><FaLinkedin/></a>
+        <a href='mailTo:shahkevin033@gmail.com' target='_blank'><GrMail/></a>
+        <a href="https://github.com/kevinshah3357" target="_blank"><SiLeetcode/></a>
+      </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
