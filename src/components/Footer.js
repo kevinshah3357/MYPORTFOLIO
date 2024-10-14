fdafa4ad0c203249012472a9dde09455e8f885dc
kevinshah3357 <135584041+kevinshah3357@users.noreply.github.com>
2024-10-14 10:46:46 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ import { SiLeetcode } from "react-icons/si";
 const Footer = () => {
   return (
     <footer>
-      <h4>Developed by Devansh Sahni</h4>
-      <h4>Copyright &copy; 2023 DS</h4>
+      <h4>Developed by Kevin Shah</h4>
+      <h4>Copyright &copy; 2024 kp</h4>
       <div className='footerLinks'>
         <a href="https://github.com/kevinshah3357" target='_blank'><FaGithub/></a>
         <a href="https://www.linkedin.com/in/kevin-shah-940a6922b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank'><FaLinkedin/></a>
